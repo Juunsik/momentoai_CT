493552a3aedb212896bbf42e58ccc5acf36b9ee3
Juunsik <Juunsik29@gmail.com>
2024-09-30 19:58:02 +0900

    MOD: 제한조건 충족

--- 제일 작은 수 제거하기.py
+++ 제일 작은 수 제거하기.py
@@ -1,10 +1,4 @@
 def solution(arr):
-    if len(arr) == 1:
-        return [-1]
 
-    min_num = min(arr)
-    count = arr.count(min_num)
-
-    for _ in range(count):
-        del arr[arr.index(min_num)]
+    del arr[arr.index(min(arr))]
     return arr if arr else [-1]
